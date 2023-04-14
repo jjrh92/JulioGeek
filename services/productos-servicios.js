422e4d6c4456e0b747c81c5043a860f53fc0e919
Julio Reyes <juliojesusreyes@gmail.com>
2023-04-14 17:58:54 -0400

    Avanzando con proyecto.

--- services/productos-servicios.js
+++ services/productos-servicios.js
@@ -2,9 +2,44 @@
 
 const listaProductos = () => {
 
-    
+    fetch ("http://localhost:3000/producto")
+    .then(respuesta => respuesta.json())
+    .catch(error => console.log (error))
 
 };
+
+
+// POST
+
+const crearProductos = (imagen, nombre, precio) => {
+
+    fetch (`http://localhost:3000/producto`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify ({
+            imagen,
+            precio,
+            nombre,
+        })
+
+    }).then (respuesta => {
+        if (respuesta.ok) {
+            return respuesta.body;
+        }
+    })
+    throw new Error ("No se pudo crear el producto");
+
+};
+
+
+export const productosServicios = {
+
+    listaProductos,
+    crearProductos
+
+}
 
 
 // Fin
